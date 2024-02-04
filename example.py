18c5ac0376179aad55ab30b46f3245ad774b53aa
Kye <kye@apacmediasolutions.com>
2024-02-03 17:01:48 -0800

    cleanup

--- example.py
+++ example.py
@@ -1,4 +1,20 @@
-from fot.main import ForestAgent
+import os
+from swarms import OpenAIChat
+from fot.main import ForestOfAgents
+from dotenv import load_dotenv
+
+# Load env
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+# create llm
+llm = OpenAIChat(openai_api_key=api_key)
 
 # Create a forest of agents
-forest = ForestAgent(llm, num_agents=5, max_loops=100, max_new_tokens=100)
+forest = ForestOfAgents(
+    llm, num_agents=5, max_loops=100, max_new_tokens=100)
+
+# Distribute tasks to the agents
+forest.distribute_tasks(
+    "What is the meaning of life?"
+)
